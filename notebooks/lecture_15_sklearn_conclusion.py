65df7cb175a3307a962030dc504b86108526b358
mlunacek <monte.lunacek@gmail.com>
2014-03-20 10:30:25 -0600

    sklearn

---
+++ notebooks/lecture_15_sklearn_conclusion.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=2>
+
+# Conclusion
+
+# <markdowncell>
+
+# - Data representation
+# - `Estimators`
+#     - LinearRegression
+#     - KNeighborsRegressor
+#     - LogisticRegression
+#     - KNeighborsClassifier
+#     - Ridge
+#     - Lasso
+#     - PCA
+#     - KMeans
+# - Bias-Variance Trade-off
+#     - Cross Validation
+#     
+
+# <headingcell level=3>
+
+# Other Important Topics
+
+# <markdowncell>
+
+# - `metrics`
+#     - classification reports
+#     - confusion matrix
+#     - f1 score
+#     - ROC curves
+#     - ROC area
+# - Other `estimators`
+#     - SVM
+#     - Trees
+#     - and many more...
+# - Kfold CV
+# - Grid search, Random search
+# - Text methods
+# - Parallel execution
+# 
+#     
+
+# <codecell>
+
+
